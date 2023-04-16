1deb5c1d637bbfd2c22a626678484d2245e346ad
andresaaap <andresaaap@hotmail.com>
2023-04-16 18:42:14 -0500

    feat: mapper for User to be interact with the db

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,4 +11,8 @@ public interface UserMapper {
     // select a user by username called getUser
     @Select("SELECT * FROM USERS WHERE username = #{username}")
     User getUser(String username);
+
+    //insert a user with a username, passwordHash, firstname, and lastname
+    @Insert("INSERT INTO USERS (username, salt, passwordHash, firstname, lastname) VALUES (#{username}, #{salt}, #{passwordHash}, #{firstname}, #{lastname})")
+    int insert(User user);
 }
