8e01ef1d85471fae6771e2e1d917b6c7c866e508
andresaaap <andresaaap@hotmail.com>
2023-04-19 17:21:50 -0500

    feat: create a new user in the signup

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -0,0 +1,49 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mappers.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class UserService {
+
+    // variable for HashService
+    private HashService hashService;
+    // variable for UserMapper
+    private UserMapper userMapper;
+
+    // constructor with parameters for HashService and UserMapper
+    public UserService(HashService hashService, UserMapper userMapper) {
+        this.hashService = hashService;
+        this.userMapper = userMapper;
+    }
+
+    // method to check if username is available
+    public boolean checkUsername(String username) {
+        // return userMapper.getUser(username) == null;
+        return userMapper.getUser(username) == null;
+    }
+
+    // create user
+    public int createUser(User newUser){
+        // check that the username doesn't already exist
+        if (checkUsername(newUser.getUsername())) {
+            SecureRandom random = new SecureRandom();
+            // generate a random salt
+            byte[] salt = new byte[16];
+            // assign random value to salt
+            random.nextBytes(salt);
+            // encode the salt to a string
+            String encodedSalt = Base64.getEncoder().encodeToString(salt);
+            // get the hash value of the password using the salt
+            String passwordSaltHash =  hashService.getHashedValue(newUser.getPassword(), encodedSalt);
+            // create the user using the userMapper
+            return userMapper.createUser(newUser.getUsername(), encodedSalt, passwordSaltHash, newUser.getFirstname(), newUser.getLastname());
+        }
+    }
+
+
+}
